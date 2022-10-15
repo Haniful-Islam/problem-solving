ddafb607d0e9a67cef02348fea7eb9f6cda86953
Haniful Islam <haniful360@gmail.com>
2022-10-15 21:59:38 +0600

    prime number and pattern added

---
+++ script.js
@@ -0,0 +1,34 @@
+// console.log(3);
+// let prime = 101;
+// let check =true;
+
+// for( let i= 2; i<prime; i++){
+//     if(prime % i == 0){
+//         check = false;
+//         break;
+//     }
+// }
+// if(check){
+//     console.log("prime")
+// }
+// else{
+//     console.log("not prime")
+// }
+let sum = 0;
+for (let i = 1; i<=50; i++){
+    sum = sum + i;
+    console.log(sum);
+}
+var n = 10 ;
+var string ="";
+
+for(var i = 1 ; i<=n ; i++){
+      for( var j = 1 ; j<=i; j++){
+            string += j;
+      }
+
+      string += "\n";
+}
+
+// console.log(string);
+
